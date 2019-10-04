0ef7a7791c2c06ca3076f394f3fcd822dd1e32d1
essential-hacker-4489 <akpojotor@yahoo.com>
2019-10-04 16:27:13 +0000

    Done.

--- index.js
+++ index.js
@@ -1,24 +1,38 @@
 function dwarfRollCall(dwarves) {
+  // create new array
   var newDwarves=[]
+  
+  // use for loop and concat method
   for(var i=0;i<dwarves.length;i++){
     newDwarves=newDwarves.concat(`${i+1}. ${dwarves[i]} `)
   }
+  // return newDwarves array
   return newDwarves.join('')
 }
 
+
 function summonCaptainPlanet(planeteerCalls){
-  var newPlanet=[]
-  planeteerCalls.forEach(function(planet){
-    newPlanet.push(`${planet.toUpperCase()}!`)
-  })
-  // for(var i=0;i<planeteerCalls.length;i++){
-  //   newPlanet=newPlanet.concat(`${planeteerCalls[i].toUpperCase()}!`)
-  // }
-  return newPlanet
+  // create new array
+  var newPlaneteerCalls=[]
+  
+  // this approach uses both forEach and push methods
+  // planeteerCalls.forEach(function(planet){
+  //   newPlaneteerCalls.push(`${planet.toUpperCase()}!`)
+  // })
+  
+  // this approach uses for loop and concat method
+  for(var i=0;i<planeteerCalls.length;i++){
+    newPlaneteerCalls=newPlaneteerCalls.concat(`${planeteerCalls[i].toUpperCase()}!`)
+  }
+  return newPlaneteerCalls
 }
 
+
 function longPlaneteerCalls(words) {
+  // loop through each word in words array
   for(var i=0;i<words.length;i++){
+    
+    // check for length of each word
     if(words[i].length>4){
       return true
     }
@@ -26,9 +40,15 @@ function longPlaneteerCalls(words) {
   return false
 }
 
+
 function findTheCheese (foods) {
+  // cheese array
   var cheese=['cheddar','gouda','camembert']
+  
+  // for loop and includes methods
   for(var i=0;i<foods.length;i++){
+    
+    // check if each element in foods array is in cheese array
     if(cheese.includes(foods[i])){
       return foods[i]
     }
